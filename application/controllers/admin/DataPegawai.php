ad359150e8e15432e81ad5bb5fa589132d5de67f
vikry234 <vikry234@gmail.com>
2023-05-23 13:01:56 +0700

    Tugas Akhir Kelompok 1

---
+++ application/controllers/admin/DataPegawai.php
@@ -0,0 +1,16 @@
+<?php  
+
+/**
+ * 
+ */
+class dataPegawai extends CI_Controller
+{
+	public function index()
+	{
+		$data = $this->db->query("SELECT * FROM data_pegawai")->result();
+		var_dump($data);
+	}
+}
+
+
+?>
